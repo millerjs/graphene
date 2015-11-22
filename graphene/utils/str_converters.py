ed03fcc2715bff62596024b04c004d17fbf901f4
Joshua Miller <jshuasmiller@gmail.com>
2015-11-22 17:53:28 -0600

    fix(to_camel_case): make noop

--- graphene/utils/str_converters.py
+++ graphene/utils/str_converters.py
@@ -1,13 +1,9 @@
 import re
 
 
-# From this response in Stackoverflow
-# http://stackoverflow.com/a/19053800/1072990
 def to_camel_case(snake_str):
-    components = snake_str.split('_')
-    # We capitalize the first letter of each component except the first one
-    # with the 'title' method and join them together.
-    return components[0] + "".join(x.title() for x in components[1:])
+    # To maintain consistency with existing snake case
+    return snake_str
 
 
 # From this response in Stackoverflow
